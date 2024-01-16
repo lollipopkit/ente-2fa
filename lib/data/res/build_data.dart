702d5087b69ca548fa33a3a02df62172676eb0db
lollipopkit <lollipopkit@icloud.com>
2024-01-16 16:24:27 +0800

    fix: ios entitlements

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,8 +2,8 @@
 
 class BuildData {
   static const String name = "2fa";
-  static const int build = 5;
+  static const int build = 6;
   static const String engine = "3.16.7";
-  static const String buildAt = "2024-01-16 16:01:49";
-  static const int modifications = 24;
+  static const String buildAt = "2024-01-16 16:17:48";
+  static const int modifications = 2;
 }
